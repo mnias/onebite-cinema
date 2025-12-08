8df85115cfdf0926dcfcaa135bd19f14d1f1b911
Park GyeongHwan <mythos2495@gmail.com>
2025-12-09 02:43:13 +0900

    일일 챌린지 2

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+export default function SearchPage() {
+  const router = useRouter();
+  const { q } = router.query;
+
+  return <h1>{`검색 결과 : ${q ?? ""}`}</h1>;
+}
